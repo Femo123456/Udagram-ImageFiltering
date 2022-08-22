b4718a70de0d27f9b3958dd4d3322f80931137c6
Faheem Ismail <femo.ismail@gmail.com>
2022-08-22 23:47:48 +0200

    updated

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+//import {Router, Request, Response} from 'express';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
@@ -32,18 +33,16 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   app.get("/filteredimage", async (req, res) => {
     const image_url = req.query.image_url.toString();
     if (!image_url) {
-      res.status(400)
-                    .send("Image URL is required")
+      res.status(400).send("Image URL is required")
     }
 
     const filteredImage = await filterImageFromURL(image_url);
 
-    res.status(200)
-                  .sendFile(filteredImage, () => {
+    res.status(200).sendFile(filteredImage, () => {
+    deleteLocalFiles([filteredImage]);
+    });
 
-                  deleteLocalFiles([filteredImage]);
-                })
-  })
+  });
 
   //! END @TODO1
   
